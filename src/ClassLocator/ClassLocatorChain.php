fc0a1cd4ebd742a3a9072cab012bce676eef7cb8
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-09-14 01:52:17 +0300

    Make $classLocators in ClassLocatorChain required

--- src/ClassLocator/ClassLocatorChain.php
+++ src/ClassLocator/ClassLocatorChain.php
@@ -16,7 +16,7 @@ final class ClassLocatorChain implements ClassLocator
      * @param iterable<ClassLocator> $classLocators
      */
     public function __construct(
-        private readonly iterable $classLocators = [],
+        private readonly iterable $classLocators,
     ) {}
 
     public function locateClass(string $name): ?Resource
