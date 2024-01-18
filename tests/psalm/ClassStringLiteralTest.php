26bd69460d298dba605627d9102848eae6d78d81
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-01-19 01:55:53 +0300

    Improved ClassStringLiteralTest

--- tests/psalm/ClassStringLiteralTest.php
+++ tests/psalm/ClassStringLiteralTest.php
@@ -7,5 +7,12 @@ namespace Typhoon\Type\ClassStringLiteralTest;
 use Typhoon\Type\ClassStringLiteralType;
 use function Typhoon\Type\extractType;
 
-/** @psalm-check-type-exact $_classLiteral = stdClass::class */
-$_classLiteral = extractType(new ClassStringLiteralType(\stdClass::class));
+/** @psalm-check-type-exact $_classStringLiteral = stdClass::class */
+$_classStringLiteral = extractType(new ClassStringLiteralType(\stdClass::class));
+
+/**
+ * @param ClassStringLiteralType<\Iterator::class|\IteratorAggregate::class> $_type
+ */
+function testClassStringLiteralIsCovariant(ClassStringLiteralType $_type): void {}
+
+testClassStringLiteralIsCovariant(new ClassStringLiteralType(\Iterator::class));
