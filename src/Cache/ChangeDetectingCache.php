b6e96d5fbd85cda9dd62306a530fa2eb3045a3c8
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-02-19 19:57:06 +0300

    Added ChangeDetectingCache

---
+++ src/Cache/ChangeDetectingCache.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Reflection\Cache;
+
+use Psr\SimpleCache\CacheInterface;
+use Typhoon\Reflection\Metadata\RootMetadata;
+
+/**
+ * @api
+ * @psalm-suppress MixedAssignment
+ */
+final class ChangeDetectingCache implements CacheInterface
+{
+    public function __construct(
+        private readonly CacheInterface $cache,
+    ) {}
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        $value = $this->cache->get($key, $default);
+
+        if ($value instanceof RootMetadata && $value->changed()) {
+            return $default;
+        }
+
+        return $value;
+    }
+
+    public function set(string $key, mixed $value, null|\DateInterval|int $ttl = null): bool
+    {
+        return $this->cache->set($key, $value, $ttl);
+    }
+
+    public function delete(string $key): bool
+    {
+        return $this->cache->delete($key);
+    }
+
+    public function clear(): bool
+    {
+        return $this->cache->clear();
+    }
+
+    /**
+     * @param iterable<string> $keys
+     * @return array<string, mixed>
+     */
+    public function getMultiple(iterable $keys, mixed $default = null): iterable
+    {
+        $values = [];
+
+        foreach ($this->cache->getMultiple($keys) as $key => $value) {
+            if ($value instanceof RootMetadata && $value->changed()) {
+                $value = $default;
+            }
+
+            $values[$key] = $value;
+        }
+
+        return $values;
+    }
+
+    public function setMultiple(iterable $values, null|\DateInterval|int $ttl = null): bool
+    {
+        return $this->cache->setMultiple($values, $ttl);
+    }
+
+    public function deleteMultiple(iterable $keys): bool
+    {
+        return $this->cache->deleteMultiple($keys);
+    }
+
+    public function has(string $key): bool
+    {
+        $value = $this->cache->get($key);
+
+        if ($value instanceof RootMetadata && $value->changed()) {
+            return false;
+        }
+
+        return true;
+    }
+}
